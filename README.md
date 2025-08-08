14acde27b93a22febb1a5109f8b4006341883f88
Bengt Brodersen <bengt.brodersen@gmail.com>
2025-08-08 11:57:52 +0200

    Update background.js

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # chrome-tab-toggle-extension
-This extension adds a shortcut to toggle through recent active tabs.
+This extension adds a shortcut to toggle between current and recent active tab.
 
 [![Available in the Chrome Web Store](https://storage.googleapis.com/chrome-gcs-uploader.appspot.com/image/WlD8wC6g8khYWPJUsQceQkhXSlv1/tbyBjqi7Zu733AAKA5n4.png "Available in the Chrome Web Store")](https://chrome.google.com/webstore/detail/tab-toggle/manalafhhpmppbabiccknagjcpgmdhpp)
 
 #### Shortcuts
 chrome://extensions/shortcuts
-* Toggle through Recent Active Tabs
+* Toggle between current and recent active tab
   * **ALT Q**
   * `MacOS` **⌥ Q**
 
--- background.js
+++ background.js
@@ -1,123 +1,22 @@
-function addPromiseSupportToStorageArea(storageArea) {
-  if(!storageArea._promiseSupport){
-    Object.getOwnPropertyNames(storageArea)
-      .filter(property => typeof storageArea[property] === 'function')
-      .forEach(functionName => {
-        const originFunctionName = `_${functionName}`
-        storageArea[originFunctionName] = storageArea[functionName]
-        storageArea[functionName] = (...args) => { 
-          if(typeof args[args.length - 1] === 'function'){
-            return storageArea[originFunctionName](...args)
-          }
-          return new Promise((resolve, reject) => {
-          storageArea[originFunctionName](...args, (...args) => {
-            if (chrome.runtime.lastError) {
-              return reject(chrome.runtime.lastError)
-            }
-            resolve(...args)
-          })
-        })}
-      })
-      storageArea._promiseSupport = true
-    }
+async function toggleTabs() {  
+  const recentTabs = await getRecentTabs();
+  if (recentTabs.length > 1) {
+    const previousTab = recentTabs[1];
+    await chrome.windows.update(previousTab.windowId, { focused: true });
+    await chrome.tabs.update(previousTab.id, { active: true });
+  }
 }
 
-async function storageAreaProxy(cache, storageArea) {
-  addPromiseSupportToStorageArea(storageArea)
-  Object.assign(cache, await storageArea.get())
-  return new Proxy(cache, {
-    get: function(target, property, receiver) {
-      return Reflect.get(target, property, receiver);
-    },
-    set: function(target, property, value, receiver) {
-      storageArea.set({
-        [property]: value
-      })
-      Reflect.set(target, property, value, receiver)
-      return true;
-    }
-  })
+function getRecentTabs() {
+  return chrome.tabs.query({})
+    .then((tabs) => tabs.sort((a, b) => a.lastAccessed > b.lastAccessed ? -1 : 1));
 }
 
-  
 // -----------------------------------------------------------------------------
 
-;(async function main() {
-  
-  const storage = await storageAreaProxy({}, chrome.storage.local)
-
-
-  async function toggleTabs() {
-    let [activeTab] = await chrome.tabs.query({
-      currentWindow: true,
-      active: true
-    })
-
-    let targetTab = activeTab ? storage.previousTab : storage.recentTab
-    if (targetTab) {
-      await chrome.windows.update(targetTab.windowId, {
-        focused: true
-      })
-      return chrome.tabs.update(targetTab.id, {
-        active: true
-      })
-    }
+chrome.commands.onCommand.addListener(async (command) => {
+  switch (command) {
+    case 'toggle-tabs':
+      return toggleTabs();
   }
-
-  async function onTabFocusChanged(windowId, tabId) {
-    console.debug("tab focus changed")
-    if (!storage.recentTab || storage.recentTab.id !== tabId) {
-      console.debug("recent tab changed")
-      storage.previousTab = storage.recentTab
-      storage.recentTab = {
-        id: tabId,
-        windowId: windowId
-      }
-    }
-  }
-
-  async function onTabClosed(windowId, tabId) {
-    console.debug("tab closed");
-    if (storage.recentTab.id === tabId) {
-      storage.recentTab = storage.previousTab
-    } else if (storage.previousTab.id === tabId) {
-      storage.previousTab = undefined
-    }
-  }
-
-
-  // -----------------------------------------------------------------------------
-
-  chrome.tabs.onActivated.addListener(async (info) => {
-    return onTabFocusChanged(info.windowId, info.tabId)
-  })
-
-  chrome.windows.onFocusChanged.addListener(async (windowId) => {
-    if (windowId === chrome.windows.WINDOW_ID_NONE) {
-      return
-    }
-
-    let [activeTab] = await chrome.tabs.query({
-      windowId: windowId,
-      active: true
-    })
-
-    return onTabFocusChanged(windowId, activeTab.id)
-  })
-
-  chrome.tabs.onRemoved.addListener(async (tabId, info) => {
-    return onTabClosed(info.windowId, tabId)
-  })
-
-
-  // -----------------------------------------------------------------------------
-
-  chrome.commands.onCommand.addListener(async (command) => {
-    console.debug(`command: ${command}`)
-    switch (command) {
-      case 'toggle-tabs':
-        return toggleTabs()
-    }
-  })
-  
-})()
+});
